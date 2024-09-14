1023d66c682a1b70480bed9ff6b4abbd99bd68ee
hong <hong@hongui-MacBookAir.local>
2024-09-14 19:02:46 +0900

    get all room api

--- src/main/java/com/example/real_chat/controller/RoomController.java
+++ src/main/java/com/example/real_chat/controller/RoomController.java
@@ -2,6 +2,7 @@ package com.example.real_chat.controller;
 
 import com.example.real_chat.dto.CreateRoomRequestDTO;
 import com.example.real_chat.dto.CreateRoomResponseDTO;
+import com.example.real_chat.dto.RoomResponseDTO;
 import com.example.real_chat.entity.Room;
 import com.example.real_chat.service.RoomService;
 
@@ -9,9 +10,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +31,15 @@ public class RoomController {
         Room room = Room.createRoom(createRoomRequestDTO.getName());
         Long roomId = roomService.addRoom(room);
         return ResponseEntity.ok().body(new CreateRoomResponseDTO(roomId));
+    }
+
+    @GetMapping("api/v1/room")
+    public ResponseEntity<List<RoomResponseDTO>> getAllRooms() {
+        List<Room> rooms = roomService.getAllRooms();
+        List<RoomResponseDTO> collect =  rooms.stream()
+                .filter(m -> m.isDeleted() == false)
+                .map(m-> new RoomResponseDTO(m.getId(), m.getName()))
+                .toList();
+        return ResponseEntity.ok().body(collect);
     }
 }
---
+++ src/main/java/com/example/real_chat/dto/RoomResponseDTO.java
@@ -0,0 +1,12 @@
+package com.example.real_chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RoomResponseDTO {
+
+    private Long id;
+    private String name;
+}
