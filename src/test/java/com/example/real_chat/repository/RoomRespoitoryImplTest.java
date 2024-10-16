8b14950f3afd315a9e5130511a17a843a29fb0f0
Joon9750 <junhyeok2166@daum.net>
2024-10-16 16:54:13 +0900

    [Add] delete test 구체화

--- src/test/java/com/example/real_chat/repository/RoomRespoitoryImplTest.java
+++ src/test/java/com/example/real_chat/repository/RoomRespoitoryImplTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.List;
 
@@ -47,7 +48,7 @@ class RoomRespoitoryImplTest {
     }
 
     @Test
-    @DisplayName("데이터를 삭제하면 DB에서 완전히 삭제되지 않고 isdeleted 필드가 true로 됩니다. DB에서 지워지지 않았기 때문에 findById로 조회됩니다.")
+    @DisplayName("데이터를 삭제하면 DB에서 완전히 삭제되지 않고 isdeleted 필드가 true로 된다.")
     void delete() {
         // when
         roomRespoitory.delete(chatRoom1.getId());
@@ -56,7 +57,38 @@ class RoomRespoitoryImplTest {
         // then
         assertTrue(chatRoom1.isDeleted());
         assertFalse(chatRoom2.isDeleted());
+    }
+
+    @Test
+    @DisplayName("데이터를 삭제해도 DB에서 완전히 지워지지 않아서 getId로 조회 가능하다.")
+    void getIdAfterDelete() {
+        // when
+        roomRespoitory.delete(chatRoom1.getId());
+        ChatRoom chatRoom1 = roomRespoitory.findById(this.chatRoom1.getId());
+
+        // then
         assertEquals(chatRoom1.getId(), this.chatRoom1.getId());
+    }
+
+    @Test
+    @DisplayName("데이터가 삭제되면 삭제된 시간이 자동으로 갱신된다.")
+    void checkDeleteTimeAfterDelete() {
+        // when
+        roomRespoitory.delete(chatRoom1.getId());
+
+        // then
+        assertNotNull(roomRespoitory.findById(this.chatRoom1.getId()).getDeletedAt());
+        assertNull(roomRespoitory.findById(this.chatRoom2.getId()).getDeletedAt());
+    }
+
+    @Test()
+    @DisplayName("이미 delete 처리된 데이터를 다시 delete하면 RuntimeException 던진다.")
+    void checkDeleteMethodRuntimeException() {
+        // when
+        roomRespoitory.delete(chatRoom1.getId());
+
+        // then
+        assertThrows(RuntimeException.class, () -> roomRespoitory.delete(chatRoom1.getId()));
     }
 
     @Test
