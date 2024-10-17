e6ff54a72b296444b8a482a5f19949fd2bf1b954
Joon9750 <junhyeok2166@daum.net>
2024-10-17 20:24:17 +0900

    [Add] implement UserService

---
+++ src/main/java/com/example/real_chat/service/UserService.java
@@ -0,0 +1,41 @@
+package com.example.real_chat.service;
+
+import com.example.real_chat.entity.User;
+import com.example.real_chat.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Long addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        if (user.isDeleted()) throw new RuntimeException();
+        else userRepository.deleteById(userId);
+    }
+
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId).orElseThrow();
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow();
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll().orElseThrow();
+    }
+
+    public List<User> getUndeletedUsers() {
+        return userRepository.findUnDeletedUsers().orElseThrow();
+    }
+}
