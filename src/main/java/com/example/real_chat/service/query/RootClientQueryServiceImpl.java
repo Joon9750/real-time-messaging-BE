5f9679a77785b71e4bfeb4b4bd2858a177cb7d93
Joon9750 <junhyeok2166@daum.net>
2024-10-18 18:16:21 +0900

    [Improve] RootClient 분리

---
+++ src/main/java/com/example/real_chat/service/query/RootClientQueryServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.real_chat.service.query;
+
+import com.example.real_chat.entity.rootClient.RootClient;
+import com.example.real_chat.repository.RootClientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class RootClientQueryServiceImpl implements RootClientQueryService {
+
+    private final RootClientRepository rootClientRepository;
+
+    public RootClient getRootClient(Long id) {
+        return rootClientRepository.findById(id).orElseThrow();
+    }
+}
