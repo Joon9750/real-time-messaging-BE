07b60ab5dff4e9193d10839b79f03738e6ff4778
Joon9750 <junhyeok2166@daum.net>
2024-10-18 19:31:05 +0900

    [Add] implement UserChatRoomRepositorylmpl

--- src/main/java/com/example/real_chat/repository/UserChatRoomRepositorylmpl.java
+++ src/main/java/com/example/real_chat/repository/UserChatRoomRepositorylmpl.java
@@ -3,6 +3,8 @@ package com.example.real_chat.repository;
 import com.example.real_chat.entity.room.ChatRoom;
 import com.example.real_chat.entity.user.User;
 import com.example.real_chat.entity.user.UserChatRoom;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,23 +12,38 @@ import java.util.List;
 @Repository
 public class UserChatRoomRepositorylmpl implements UserChatRoomRepository {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public Long save(UserChatRoom userChatRoom) {
-        return 0;
+        entityManager.persist(userChatRoom);
+        return userChatRoom.getId();
     }
 
     @Override
     public List<UserChatRoom> findByUser(User user) {
-        return List.of();
+        return entityManager.createQuery(
+                "SELECT uc FROM UserChatRoom uc WHERE uc.user = :user", UserChatRoom.class)
+                .setParameter("user", user)
+                .getResultList();
     }
 
     @Override
     public List<UserChatRoom> findByChatRoom(ChatRoom chatRoom) {
-        return List.of();
+        return entityManager.createQuery(
+                "select uc from UserChatRoom uc where uc.chatRoom = :chatRoom",UserChatRoom.class)
+                .setParameter("chatRoom", chatRoom)
+                .getResultList();
     }
 
     @Override
     public boolean existsByUserAndChatRoom(User user, ChatRoom chatRoom) {
-        return false;
+        return (boolean) entityManager.createQuery(
+                "select case when count(uc) > 0 then true else false end" +
+                        " from UserChatRoom uc where uc.user = :user and uc.chatRoom = :chatRoom")
+                .setParameter("user", user)
+                .setParameter("chatRoom", chatRoom)
+                .getSingleResult();
     }
 }
