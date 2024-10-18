bdfe8e38fcb7e152b30dcc20ba4063830761b9c2
Joon9750 <junhyeok2166@daum.net>
2024-10-18 20:24:20 +0900

    [Add] deleteUserChatRoom 기능 구현

--- src/main/java/com/example/real_chat/repository/UserChatRoomRepository.java
+++ src/main/java/com/example/real_chat/repository/UserChatRoomRepository.java
@@ -5,10 +5,13 @@ import com.example.real_chat.entity.user.User;
 import com.example.real_chat.entity.userChatRoom.UserChatRoom;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserChatRoomRepository {
 
     Long save(UserChatRoom userChatRoom);
+
+    Optional<UserChatRoom> findByUserAndChatRoom(User user, ChatRoom chatRoom);
 
     // 특정 유저가 참여한 채팅방 목록 조회
     List<UserChatRoom> findByUser(User user);
@@ -18,4 +21,6 @@ public interface UserChatRoomRepository {
 
     // 특정 유저가 특정 채팅방에 참여 여부 확인
     boolean existsByUserAndChatRoom(User user, ChatRoom chatRoom);
+
+    void delete(UserChatRoom userChatRoom);
 }
