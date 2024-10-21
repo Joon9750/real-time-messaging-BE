2cc76d70e59c7897b2153c90b05994a77866dcf8
Joon9750 <junhyeok2166@daum.net>
2024-10-21 14:50:03 +0900

    [Add] UserChatRoomQueryService 구현

--- src/main/java/com/example/real_chat/service/query/UserChatRoomQueryService.java
+++ src/main/java/com/example/real_chat/service/query/UserChatRoomQueryService.java
@@ -1,4 +1,14 @@
 package com.example.real_chat.service.query;
 
-public class UserChatRoomQueryService {
+
+import com.example.real_chat.entity.userChatRoom.UserChatRoom;
+
+import java.util.List;
+
+public interface UserChatRoomQueryService {
+
+    UserChatRoom findUserInChatRoom(Long userId, Long chatRoomId);
+    List<UserChatRoom> getChatRoomsUserParticipatesIn(Long userId);
+    List<UserChatRoom> getParticipantsInChatRoom(Long chatRoomId);
+    Boolean doesUserExistInChatRoom(Long userId, Long chatRoomId);
 }
