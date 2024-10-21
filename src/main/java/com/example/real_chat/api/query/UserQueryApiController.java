7965758c02c41320c384302bc088c19f623d6b08
Joon9750 <junhyeok2166@daum.net>
2024-10-21 15:49:32 +0900

    [Fix] DTO 명칭 수정, 마지막에 Dto 삭제

--- src/main/java/com/example/real_chat/api/query/UserQueryApiController.java
+++ src/main/java/com/example/real_chat/api/query/UserQueryApiController.java
@@ -1,7 +1,7 @@
 package com.example.real_chat.api.query;
 
 import com.example.real_chat.dto.common.Result;
-import com.example.real_chat.dto.user.response.GetUserResponseDto;
+import com.example.real_chat.dto.user.response.GetUserResponse;
 import com.example.real_chat.entity.user.User;
 import com.example.real_chat.service.query.UserQueryService;
 import lombok.RequiredArgsConstructor;
@@ -21,28 +21,28 @@ public class UserQueryApiController {
     private final UserQueryService userQueryService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<GetUserResponseDto> getUserById(
+    public ResponseEntity<GetUserResponse> getUserById(
             @PathVariable Long id
     ) {
         User user = userQueryService.getUserById(id);
-        return ResponseEntity.ok().body(new GetUserResponseDto(user.getId(), user.getUserName()));
+        return ResponseEntity.ok().body(new GetUserResponse(user.getId(), user.getUserName()));
     }
 
     @GetMapping("/all")
-    public ResponseEntity<Result<?>> getAllUsers() {
+    public ResponseEntity<Result<List<GetUserResponse>>> getAllUsers() {
         List<User> users = userQueryService.getAllUsers();
-        List<GetUserResponseDto> response = users.stream()
-                .map(m -> new GetUserResponseDto(m.getId(), m.getUserName()))
+        List<GetUserResponse> response = users.stream()
+                .map(m -> new GetUserResponse(m.getId(), m.getUserName()))
                 .toList();
 
         return ResponseEntity.ok().body(new Result<>(response));
     }
 
     @GetMapping("/undeleted")
-    public ResponseEntity<Result<?>> getUndeletedUsers() {
+    public ResponseEntity<Result<List<GetUserResponse>>> getUndeletedUsers() {
         List<User> users = userQueryService.getUndeletedUsers();
-        List<GetUserResponseDto> response = users.stream()
-                .map(m -> new GetUserResponseDto(m.getId(), m.getUserName()))
+        List<GetUserResponse> response = users.stream()
+                .map(m -> new GetUserResponse(m.getId(), m.getUserName()))
                 .toList();
 
         return ResponseEntity.ok().body(new Result<>(response));
