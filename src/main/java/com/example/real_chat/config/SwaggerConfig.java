d0d1ee6f3cddf9bb869b21849e71beed5f613d10
Joon9750 <junhyeok2166@daum.net>
2024-10-21 16:24:47 +0900

    [Add] SwaggerConfig 구현

---
+++ src/main/java/com/example/real_chat/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.example.real_chat.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    // 데이터베이스 로드 이후
+    // http://localhost:8080/swagger-ui/index.html#/ url로 실행하면 스웨거 페이지 뜸
+    
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Spring Boot REST API Specifications")
+                .description("Specification")
+                .version("1.0.0");
+    }
+}
