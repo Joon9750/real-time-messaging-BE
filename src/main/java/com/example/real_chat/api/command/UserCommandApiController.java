1a08e126d467b00a61197e9708fc5c378eb547f7
Joon9750 <junhyeok2166@daum.net>
2024-10-21 17:28:28 +0900

    [Fix] swagger에 뜨지 않는 문제 해결

--- src/main/java/com/example/real_chat/api/command/UserCommandApiController.java
+++ src/main/java/com/example/real_chat/api/command/UserCommandApiController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/command/user")
 public class UserCommandApiController {
