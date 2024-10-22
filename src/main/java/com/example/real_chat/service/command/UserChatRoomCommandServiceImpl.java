2759a9d6d3721a22f0b90a74fc1814a061b9d515
Joon9750 <junhyeok2166@daum.net>
2024-10-22 14:16:15 +0900

    [Chore] 불필요 주석 제거

--- src/main/java/com/example/real_chat/service/command/UserChatRoomCommandServiceImpl.java
+++ src/main/java/com/example/real_chat/service/command/UserChatRoomCommandServiceImpl.java
@@ -28,23 +28,19 @@ public class UserChatRoomCommandServiceImpl implements UserChatRoomCommandServic
     public Long joinChatRoom(Long userId, Long chatRoomId) {
         User user = getUser(userId);
         ChatRoom chatRoom = getChatRoom(chatRoomId);
-
-        // 유저가 이미 해당 채팅방에 속해 있는지 확인
+        
         boolean isUserInChatRoom = userChatRoomRepository.existsByUserAndChatRoom(user, chatRoom);
 
         if (!isUserInChatRoom) {
-            // 유저가 채팅방에 속해 있지 않으면 새로운 UserChatRoom 생성 및 저장
             UserChatRoom userChatRoom = new UserChatRoom();
             userChatRoom.setUser(user);
             userChatRoom.setChatRoom(chatRoom);
 
-            // 연관관계 편의 메소드
             user.addUserChatRoom(userChatRoom);
 
             userChatRoomRepository.save(userChatRoom);
             return userChatRoom.getId();
         } else {
-            // 이미 참여 중이라면 필요한 다른 로직 처리 (에러 반환, 메시지 출력 등)
             throw new RuntimeException();
         }
     }
