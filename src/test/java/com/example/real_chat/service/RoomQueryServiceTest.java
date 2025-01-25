48b97b7b4cd807b9c8a1ac7793c688d808ae5154
Joon9750 <junhyeok2166@daum.net>
2025-01-25 14:36:51 +0900

    [Add] RoomServiceTestDataBuilder로 테스트 데이터 생성 코드 중복 제거

--- src/test/java/com/example/real_chat/service/RoomQueryServiceTest.java
+++ src/test/java/com/example/real_chat/service/RoomQueryServiceTest.java
@@ -1,6 +1,9 @@
 package com.example.real_chat.service;
 
+import com.example.real_chat.entity.room.ChatRoom;
+import com.example.real_chat.entity.rootClient.RootClient;
 import com.example.real_chat.service.query.RoomQueryServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -10,4 +13,30 @@ public class RoomQueryServiceTest {
 
     @InjectMocks
     private RoomQueryServiceImpl roomQueryService;
+
+    private RootClient rootClient;
+    private ChatRoom chatRoom;
+
+    @BeforeEach
+    void setUp() {
+        rootClient = setUpRootClient();
+        chatRoom = setUpChatRoom(rootClient);
+    }
+
+    private ChatRoom setUpChatRoom(RootClient rootClient) {
+        return ChatRoom.builder()
+                .id(1L)
+                .name("newChatRoom")
+                .rootClient(rootClient)
+                .build();
+    }
+
+    private RootClient setUpRootClient() {
+        return RootClient.builder()
+                .id(1L)
+                .clientId("clientId")
+                .clientPassword("clientPassword")
+                .clientName("clientName")
+                .build();
+    }
 }
---
+++ src/test/java/com/example/real_chat/service/RoomServiceTestDataBuilder.java
@@ -0,0 +1,23 @@
+package com.example.real_chat.service;
+
+import com.example.real_chat.entity.room.ChatRoom;
+import com.example.real_chat.entity.rootClient.RootClient;
+
+public class RoomServiceTestDataBuilder {
+    public static RootClient createRootClient() {
+        return RootClient.builder()
+                .id(1L)
+                .clientId("clientId")
+                .clientPassword("clientPassword")
+                .clientName("clientName")
+                .build();
+    }
+
+    public static ChatRoom createChatRoom(RootClient rootClient) {
+        return ChatRoom.builder()
+                .id(1L)
+                .name("newChatRoom")
+                .rootClient(rootClient)
+                .build();
+    }
+}
