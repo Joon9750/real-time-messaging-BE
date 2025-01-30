4543983a9fdea09f3d7caea86b39f8407a0e5336
Joon9750 <junhyeok2166@daum.net>
2025-01-30 18:07:06 +0900

    [Add] ServiceTestDataBuilder static 메소드 구현

--- src/test/java/com/example/real_chat/service/ServiceTest.java
+++ src/test/java/com/example/real_chat/service/ServiceTest.java
@@ -1,12 +1,20 @@
 package com.example.real_chat.service;
 
+import com.example.real_chat.entity.room.ChatRoom;
+import com.example.real_chat.entity.rootClient.RootClient;
+import com.example.real_chat.entity.user.User;
+import com.example.real_chat.entity.userChatRoom.UserChatRoom;
 import com.example.real_chat.repository.RoomRepository;
 import com.example.real_chat.repository.RootClientRepository;
 import com.example.real_chat.repository.UserChatRoomRepository;
 import com.example.real_chat.repository.UserRepository;
+import com.example.real_chat.service.builder.ServiceTestDataBuilder;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public abstract class ServiceTest {
@@ -15,4 +23,13 @@ public abstract class ServiceTest {
     @Mock protected UserRepository userRepository;
     @Mock protected RootClientRepository rootClientRepository;
     @Mock protected UserChatRoomRepository userChatRoomRepository;
+
+    protected RootClient rootClient = ServiceTestDataBuilder.createRootClient();
+    protected ChatRoom chatRoom = ServiceTestDataBuilder.createChatRoom(rootClient);
+    protected User user = ServiceTestDataBuilder.createUser(rootClient);
+    protected UserChatRoom userChatRoom;
+
+    @BeforeEach
+    void setMockitoAction() {
+    }
 }
--- src/test/java/com/example/real_chat/service/room/RoomQueryServiceTest.java
+++ src/test/java/com/example/real_chat/service/room/RoomQueryServiceTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 
 import java.util.Optional;
 
