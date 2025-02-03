ba4554021d0c7ca8e169f0a39f5471a6bc148935
Joon9750 <junhyeok2166@daum.net>
2025-02-03 15:14:14 +0900

    [Add] UserQueryServiceTest 클래스 테스트 메소드 명시

--- src/test/java/com/example/real_chat/service/user/UserQueryServiceTest.java
+++ src/test/java/com/example/real_chat/service/user/UserQueryServiceTest.java
@@ -2,10 +2,30 @@ package com.example.real_chat.service.user;
 
 import com.example.real_chat.service.global.ServiceTest;
 import com.example.real_chat.service.query.UserQueryServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 
 public class UserQueryServiceTest extends ServiceTest {
 
     @InjectMocks
     private UserQueryServiceImpl userQueryService;
+
+    @Test
+    @DisplayName("유저 조회 테스트")
+    void testGetUserSuccess() {
+
+    }
+
+    @Test
+    @DisplayName("유저 조회 테스트 - 조회할 유저가 없을 경우")
+    void testGetUserFailure_WhenUserNotFound() {
+
+    }
+
+    @Test
+    @DisplayName("전체 유저 조회 테스트")
+    void testGetAllUserSuccess() {
+
+    }
 }
