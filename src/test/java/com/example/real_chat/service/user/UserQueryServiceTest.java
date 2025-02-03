681ae2131f2844ff2ae439b2dd44022cb9d3649f
Joon9750 <junhyeok2166@daum.net>
2025-02-03 15:02:10 +0900

    [Add] UserQueryServiceTest 생성

---
+++ src/test/java/com/example/real_chat/service/user/UserQueryServiceTest.java
@@ -0,0 +1,11 @@
+package com.example.real_chat.service.user;
+
+import com.example.real_chat.service.global.ServiceTest;
+import com.example.real_chat.service.query.UserQueryServiceImpl;
+import org.mockito.InjectMocks;
+
+public class UserQueryServiceTest extends ServiceTest {
+
+    @InjectMocks
+    private UserQueryServiceImpl userQueryService;
+}
