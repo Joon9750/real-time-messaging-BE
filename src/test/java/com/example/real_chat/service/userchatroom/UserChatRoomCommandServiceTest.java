44c0810bfbca0b9daeb77a185b02528559aebc45
Joon9750 <junhyeok2166@daum.net>
2025-02-24 10:49:11 +0900

    [Add] UserChatRoomCommandServiceTest 클래스 생성

---
+++ src/test/java/com/example/real_chat/service/userchatroom/UserChatRoomCommandServiceTest.java
@@ -0,0 +1,11 @@
+package com.example.real_chat.service.userchatroom;
+
+import com.example.real_chat.service.command.UserChatRoomCommandServiceImpl;
+import com.example.real_chat.service.global.ServiceTest;
+import org.mockito.InjectMocks;
+
+public class UserChatRoomCommandServiceTest extends ServiceTest {
+
+    @InjectMocks
+    private UserChatRoomCommandServiceImpl userChatRoomCommandService;
+}
